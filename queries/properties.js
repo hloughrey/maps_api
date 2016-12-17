abe2863fb7200e9afe572479515cd109ff0da04f
Hugh Loughrey <hugh.loughrey@gmail.com>
2016-12-17 16:24:08 +0000

    Break url over multiple lines

--- queries/properties.js
+++ queries/properties.js
@@ -4,23 +4,29 @@ const credentials = require('../bin/credentials');
 module.exports = {
 
   getLatest: function(req, res) {
-    let url = `${credentials.zoopla.url}/property_listings.json?postcode=${req.query.postcode}&page_size=${req.query.page_size}&property_type=${req.query.property_type}&api_key=${credentials.zoopla.api_key}`;
+    let url = `${credentials.zoopla.url}/property_listings.json?
+    postcode=${req.query.postcode}&
+    page_size=${req.query.page_size}&
+    property_type=${req.query.property_type}&
+    api_key=${credentials.zoopla.api_key}`.replace(/(\n    )/g, '');
     axios
       .get (url)
       .then((response) => {
         res.status(200)
         .json({
           status: 'Success',
+          statusCode: 200,
           message: 'Smashing',
           data: response.data
         });
       })
       .catch((err) => {
-        console.log('Error: ', err);
-        res.status(500)
+        res.status(err.response.status)
         .json({
           status: 'Error',
-          message: 'AHHH THERE WAS AN ERROR'
+          statusCode: err.response.status,
+          message: 'AHHH THERE WAS AN ERROR',
+          url: url
         });
       });
   },
